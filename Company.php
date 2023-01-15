265602d2a696a939c0f697ac3e290c39e9b78424
miwanoff <marina.iva.noffa@gmail.com>
2023-01-15 15:31:36 +0200

    Add Company class

---
+++ Company.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once "Employee.php";
+
+class Company
+{
+    private $name;
+    private $employees = [];
+
+    public function __construct($name = "", $employees = [])
+    {
+        $this->name = $name;
+        $this->employees = $employees;
+    }
+
+    public function get_name()
+    {
+        return $this->name;
+    }
+    public function set_name($name)
+    {
+        $this->name = $name;
+    }
+    public function get_employees()
+    {
+        return $this->employees;
+    }
+    public function set_employees(array $employees)
+    {
+        $this->employees = $employees;
+    }
+    public function set_employee($i, $employee)
+    {
+        $this->employees[$i] = $employee;
+    }
+    public function get_employee($i)
+    {
+        return $this->employees[$i];
+    }
+    public function add_employee($employee)
+    {
+        $this->employees[] = $employee;
+    }
+
+    public function get_employees_info()
+    {
+        $emps = implode(",", $this->employees);
+        return "$emps";
+    }
+
+    public function __toString()
+    {
+        return "{$this->get_name()}";
+    }
+}
--- index.php
+++ index.php
@@ -1,21 +1,25 @@
 <?php
 
 require_once "Employee.php";
-
+require_once "Company.php";
 $emp = new Employee();
 
 $emp->set_name("John");
 $emp->surname = "Johnson";
-echo $emp->get_name() . " " . $emp->get_surname();
+//echo $emp->get_name() . " " . $emp->get_surname();
 
 //$emp->age = 23;
 
 $emp->set_age(23);
 
-echo $emp->get_age();
-echo $emp->get_full_info();
-echo $emp;
+// echo $emp->get_age();
+// echo $emp->get_full_info();
+//echo $emp;
 
 $emp1 = new Employee("Nicol", "Kidman");
 $emp1->set_age(23);
-echo $emp1;
+//echo $emp1;
+$arr = [$emp, $emp1];
+$company = new Company("Kit", $arr);
+
+echo $company->get_employees_info();
